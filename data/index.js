ac5f91224c31668fa421b73c6c498a9fe1168072
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-05-16 11:58:53 -0300

    Other Gender Finished

--- data/index.js
+++ data/index.js
@@ -1,34 +1,56 @@
-function irmech(){
+function irmech(){ // Isso irá limpar todas as interações de Gênero e instalar a opção de digitar o Gênero diferente de Masculino e Feminino.
 	var conjunt = document.getElementById('conjunt');
 	var radgen = document.getElementsByName('radgen');	
 	var main = document.getElementById('main');
-	if (radgen[2].checked){
-		conjunt.remove();
-		var conjunt = document.createElement('div');
+	if (radgen[2].checked){ // Verificação para ver se a pessoa clicou em "Other".
+		conjunt.remove(); // Limpar todos os elementos do Conjunto, inclusive ele próprio.
+		var conjunt = document.createElement('div'); // Criar o conjunto novamente, só que com os dados de adicionar seu Gênero por texto.
 		conjunt.setAttribute('id', 'conjunt');
-		var back = document.createElement('input');
+		var back = document.createElement('input'); // Criar o botão "Cancelar".
 		back.setAttribute('type', 'button');
 		back.setAttribute('value', 'Cancel');
 		back.setAttribute('onclick', 'canceloth()');
-		var othergen = document.createElement('input');
+		var othergen = document.createElement('input'); // Criar a área onde podes escrever seu Gênero.
 		othergen.setAttribute('type', 'text');
 		othergen.setAttribute('value', 'My gender is...');
-		main.appendChild(conjunt);
+		main.appendChild(conjunt); // Adicionar esses três componentes no Documento (eles estavam criados apenas na memória, agora isso está os colocando na parte visual).
 		conjunt.appendChild(back);
 		conjunt.appendChild(othergen);
 	};
 };
 
-function canceloth(){
+function canceloth(){ // Se a pessoa apertar em cancelar, irá executar essa função, que deixará as opções de gênero como estava antes.
 	var conjunt = document.getElementById('conjunt');
-	conjunt.remove();
-	var conjunt = document.createElement('div');
+	conjunt.remove(); // Limpar todos os elementos do Conjunto, inclusive ele próprio.
+	var conjunt = document.createElement('div'); // Criar o conjunto novamente, só que com os dados de adicionar seu Gênero por texto.
 	conjunt.setAttribute('id', 'conjunt');
-	var radgen = document.createElement('input');
-	radgen.setAttribute('type', 'radio');
-	radgen.setAttribute('name', 'radgen');
-	radgen.setAttribute('id', 'mas');
-	main.appendChild(conjunt);
-	conjunt.appendChild(radgen);
-	//Beta...
+	var checkma = document.createElement('input'); // Checagem masculina.
+	checkma.setAttribute('type', 'radio');
+	checkma.setAttribute('name', 'radgen');
+	checkma.setAttribute('id', 'mas');
+	var lacheckma = document.createElement('label');
+	lacheckma.setAttribute('for', 'mas');
+	var checkfe = document.createElement('input'); // Checagem feminina.
+	checkfe.setAttribute('type', 'radio');
+	checkfe.setAttribute('name', 'radgen');
+	checkfe.setAttribute('id', 'fem');
+	var lacheckfe = document.createElement('label');
+	lacheckfe.setAttribute('for', 'fem');
+	var checkot = document.createElement('input'); // Checagem diferenciada.
+	checkot.setAttribute('type', 'radio');
+	checkot.setAttribute('name', 'radgen');
+	checkot.setAttribute('id', 'oth');
+	checkot.setAttribute('onclick', 'irmech()');
+	var lacheckot = document.createElement('label');
+	lacheckot.setAttribute('for', 'oth');
+	main.appendChild(conjunt); // Adicionar os componentes na parte visual.
+	conjunt.appendChild(checkma);
+	conjunt.appendChild(lacheckma);
+	lacheckma.innerHTML = ' Masculine ';
+	conjunt.appendChild(checkfe);
+	conjunt.appendChild(lacheckfe);
+	lacheckfe.innerHTML = ' Feminine ';
+	conjunt.appendChild(checkot);
+	conjunt.appendChild(lacheckot);
+	lacheckot.innerHTML = ' Other ';
 };
